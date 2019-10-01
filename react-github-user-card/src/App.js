a7d18bc848805474f1cb23fff4fb3657942ea1c6
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-01 16:36:29 -0500

    follower cards are now displayed

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 class App extends React.Component{
   state = {
-    user: []
+    user: [],
+    userFollowers: []
   };
   componentDidMount(){
     axios.get('https://api.github.com/users/WilliamBerlin76')
@@ -17,6 +18,16 @@ class App extends React.Component{
     })
     .catch(err => {
       console.log('data not retrieved', err)
+    });
+    axios.get('https://api.github.com/users/WilliamBerlin76/followers')
+    .then(res => {
+      console.log(res)
+      this.setState({
+        userFollowers: res.data
+      })
+    })
+    .catch(err => {
+      console.log('follower data not retrieved', err)
     })
   }
 
@@ -24,10 +35,21 @@ class App extends React.Component{
       return (
       <div className="App">
         <h1>Github User Cards!</h1>
-        <div>
-          <img src={this.state.user.avatar_url} alt='profile picture'/>
+        <div className="profile-card">
+          <img src={this.state.user.avatar_url} alt='Github pic'/>
           <h1>{this.state.user.name}</h1>
           <p>{this.state.user.login}</p>
+        </div>
+        <div className="follower-profile-card">
+          <h2>Followers</h2>
+          {this.state.userFollowers.map(item => {
+            return(
+              <>
+                <img src={item.avatar_url} key={item.id} alt='follower pic'/>
+                <h1 key={item.login}>{item.login}</h1>
+              </>
+            )
+          })}
         </div>
       </div>
     );
