974cd346fb8cd9bbc578bd2559d399ac158be553
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-01 17:07:37 -0500

    working on styling

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -40,17 +40,17 @@ class App extends React.Component{
           <h1>{this.state.user.name}</h1>
           <p>{this.state.user.login}</p>
         </div>
-        <div className="follower-profile-card">
-          <h2>Followers</h2>
+        <h2>Followers</h2>
+        <section className="followers-container">
           {this.state.userFollowers.map(item => {
             return(
-              <>
+              <div className='follower-card'>
                 <img src={item.avatar_url} key={item.id} alt='follower pic'/>
                 <h1 key={item.login}>{item.login}</h1>
-              </>
+              </div>
             )
           })}
-        </div>
+        </section>
       </div>
     );
   }
