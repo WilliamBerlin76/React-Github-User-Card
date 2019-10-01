9066bc4fe23f6f8023d61b08ea3ee77099134ef2
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-01 16:14:14 -0500

    my card is now displayed

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      id: null
-    }
+  state = {
+    user: []
+  };
+  componentDidMount(){
+    axios.get('https://api.github.com/users/WilliamBerlin76')
+    .then(res => {
+      console.log(res)
+      this.setState({
+        user: res.data
+     });
+    })
+    .catch(err => {
+      console.log('data not retrieved', err)
+    })
   }
+
   render() {
       return (
       <div className="App">
         <h1>Github User Cards!</h1>
+        <div>
+          <img src={this.state.user.avatar_url} alt='profile picture'/>
+          <h1>{this.state.user.name}</h1>
+          <p>{this.state.user.login}</p>
+        </div>
       </div>
     );
   }
